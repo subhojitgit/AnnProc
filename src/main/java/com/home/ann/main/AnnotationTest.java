8e074f7b7947c92975073029e9d8cb16f895df75
Subhojit Das <subhojit@amazon.com>
2018-02-07 16:30:55 -0800

    Debug enabled annotation processing

--- src/main/java/com/home/ann/main/AnnotationTest.java
+++ src/main/java/com/home/ann/main/AnnotationTest.java
@@ -2,4 +2,8 @@ package com.home.ann.main;
 
 public class AnnotationTest {
 
+    public static void main(String[] args) {
+        AnnotationHolder annotationHolder = new AnnotationHolder();
+        System.out.println(annotationHolder.toString());
+    }
 }
